e2a98155a8c0609033fb63db6975b72c039caeea
monabazzaz <mbazzaz@live.unc.edu>
2017-04-26 00:05:21 -0400

    trying to align the two major components next to eachother. no errors as far as the form goes

--- resources/assets/js/components/State.vue
+++ resources/assets/js/components/State.vue
@@ -1,104 +1,109 @@
 <template>
-  <div class="State panel panel-default">
-    <div class="panel-heading">
-      <a :href="'state:' + state.name">
-        <i class="glyphicon glyphicon-earphone"></i>
-      </a>
+  <!-- <div class="container-fluid"> -->
+    <div class= "visited">
+      <div class="State panel panel-default col-lg-8 col-md-8 col-sm-12">
+        <div class="panel-heading">
+          <a :href="'state:' + state.name">
+            <i class="glyphicon glyphicon-earphone"></i>
+          </a>
 
-      <a class="tool" href="#" @click.prevent="remove">
-        <i class="glyphicon glyphicon-remove pull-right"></i>
-      </a>
+          <a class="tool" href="#" @click.prevent="remove">
+            <i class="glyphicon glyphicon-remove pull-right"></i>
+          </a>
 
-      <a class="tool" href="#" @click.prevent="editing = true" v-show="!editing">
-        <i class="glyphicon glyphicon-pencil pull-right"></i>
-      </a>
-    </div>
-    <div class="panel-body">
-      <div class="live" v-show="!editing">
-        {{ state.name }} {{ state.year }}
+          <a class="tool" href="#" @click.prevent="editing = true" v-show="!editing">
+            <i class="glyphicon glyphicon-pencil pull-right"></i>
+          </a>
+        </div>
+        <div class="panel-body">
+          <div class="live" v-show="!editing">
+            {{ state.name }} {{ state.year }}
+          </div>
+          <div class="editing" v-show="editing">
+            <p>
+            <div class="form-group">
+              <label for="name">Edit state visited</label>
+              <select class="form-control" v-model="name">
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                  <option value="AR">Arkansas</option>
+                  <option value="CA">California</option>
+                  <option value="CO">Colorado</option>
+                  <option value="CT">Connecticut</option>
+                  <option value="DE">Delaware</option>
+                  <option value="DC">District Of Columbia</option>
+                  <option value="FL">Florida</option>
+                  <option value="GA">Georgia</option>
+                  <option value="HI">Hawaii</option>
+                  <option value="ID">Idaho</option>
+                  <option value="IL">Illinois</option>
+                  <option value="IN">Indiana</option>
+                  <option value="IA">Iowa</option>
+                  <option value="KS">Kansas</option>
+                  <option value="KY">Kentucky</option>
+                  <option value="LA">Louisiana</option>
+                  <option value="ME">Maine</option>
+                  <option value="MD">Maryland</option>
+                  <option value="MA">Massachusetts</option>
+                  <option value="MI">Michigan</option>
+                  <option value="MN">Minnesota</option>
+                  <option value="MS">Mississippi</option>
+                  <option value="MO">Missouri</option>
+                  <option value="MT">Montana</option>
+                  <option value="NE">Nebraska</option>
+                  <option value="NV">Nevada</option>
+                  <option value="NH">New Hampshire</option>
+                  <option value="NJ">New Jersey</option>
+                  <option value="NM">New Mexico</option>
+                  <option value="NY">New York</option>
+                  <option value="NC">North Carolina</option>
+                  <option value="ND">North Dakota</option>
+                  <option value="OH">Ohio</option>
+                  <option value="OK">Oklahoma</option>
+                  <option value="OR">Oregon</option>
+                  <option value="PA">Pennsylvania</option>
+                  <option value="RI">Rhode Island</option>
+                  <option value="SC">South Carolina</option>
+                  <option value="SD">South Dakota</option>
+                  <option value="TN">Tennessee</option>
+                  <option value="TX">Texas</option>
+                  <option value="UT">Utah</option>
+                  <option value="VT">Vermont</option>
+                  <option value="VA">Virginia</option>
+                  <option value="WA">Washington</option>
+                  <option value="WV">West Virginia</option>
+                  <option value="WI">Wisconsin</option>
+                  <option value="WY">Wyoming</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="year">Edit year visited</label>
+              <input class="form-control" type="text" v-model="year" />
+            </div>
+            <div class="form-group">
+              <label for="days">Edit number of days</label>
+              <input class="form-control" type="text" v-model="days" />
+            </div>
+            <div class="form-group">
+              <label for="notes">Edit your memos</label>
+              <textarea class="form-control" v-model="notes"></textarea>
+            </div>
+            <div class="form-group">
+              <label for="rating">Edit your rating</label>
+              <input class="form-control" type="text" v-model="rating" />
+            </div>
+            </p>
+            <p>
+              <button class="btn btn-success" @click="save">Save</button>
+              <button class="btn btn-default" @click="cancel">Cancel</button>
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="editing" v-show="editing">
-        <p>
-        <div class="form-group">
-          <label for="name">Edit state visited</label>
-          <select class="form-control" v-model="name">
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DE">Delaware</option>
-              <option value="DC">District Of Columbia</option>
-              <option value="FL">Florida</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="IA">Iowa</option>
-              <option value="KS">Kansas</option>
-              <option value="KY">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="ME">Maine</option>
-              <option value="MD">Maryland</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MS">Mississippi</option>
-              <option value="MO">Missouri</option>
-              <option value="MT">Montana</option>
-              <option value="NE">Nebraska</option>
-              <option value="NV">Nevada</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NY">New York</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="year">Edit year visited</label>
-          <input class="form-control" type="text" v-model="year" />
-        </div>
-        <div class="form-group">
-          <label for="days">Edit number of days</label>
-          <input class="form-control" type="text" v-model="days" />
-        </div>
-        <div class="form-group">
-          <label for="notes">Edit your memos</label>
-          <textarea class="form-control" v-model="notes"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="rating">Edit your rating</label>
-          <input class="form-control" type="text" v-model="rating" />
-        </div>
-        </p>
-        <p>
-          <button class="btn btn-success" @click="save">Save</button>
-          <button class="btn btn-default" @click="cancel">Cancel</button>
-        </p>
-      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12"></div>
     </div>
-  </div>
+  <!-- </div> -->
 </template>
 <script>
 import axios from 'axios';
@@ -180,6 +185,13 @@ export default {
 </script>
 
 <style>
+
+.visited {
+  float: right; width: 100%;
+  position: relative;
+  display: inline-block;
+}
+
 .tool > i {
   margin-left: 15px;
 }
--- resources/assets/js/components/StateForm.vue
+++ resources/assets/js/components/StateForm.vue
@@ -1,79 +1,84 @@
 <template>
-  <div class="StateForm">
-    <div class="form-group">
-    <label for="name">Select a state</label>
-    <select class="form-control" v-model="name">
-        <option value="AL">Alabama</option>
-        <option value="AK">Alaska</option>
-        <option value="AZ">Arizona</option>
-        <option value="AR">Arkansas</option>
-        <option value="CA">California</option>
-        <option value="CO">Colorado</option>
-        <option value="CT">Connecticut</option>
-        <option value="DE">Delaware</option>
-        <option value="DC">District Of Columbia</option>
-        <option value="FL">Florida</option>
-        <option value="GA">Georgia</option>
-        <option value="HI">Hawaii</option>
-        <option value="ID">Idaho</option>
-        <option value="IL">Illinois</option>
-        <option value="IN">Indiana</option>
-        <option value="IA">Iowa</option>
-        <option value="KS">Kansas</option>
-        <option value="KY">Kentucky</option>
-        <option value="LA">Louisiana</option>
-        <option value="ME">Maine</option>
-        <option value="MD">Maryland</option>
-        <option value="MA">Massachusetts</option>
-        <option value="MI">Michigan</option>
-        <option value="MN">Minnesota</option>
-        <option value="MS">Mississippi</option>
-        <option value="MO">Missouri</option>
-        <option value="MT">Montana</option>
-        <option value="NE">Nebraska</option>
-        <option value="NV">Nevada</option>
-        <option value="NH">New Hampshire</option>
-        <option value="NJ">New Jersey</option>
-        <option value="NM">New Mexico</option>
-        <option value="NY">New York</option>
-        <option value="NC">North Carolina</option>
-        <option value="ND">North Dakota</option>
-        <option value="OH">Ohio</option>
-        <option value="OK">Oklahoma</option>
-        <option value="OR">Oregon</option>
-        <option value="PA">Pennsylvania</option>
-        <option value="RI">Rhode Island</option>
-        <option value="SC">South Carolina</option>
-        <option value="SD">South Dakota</option>
-        <option value="TN">Tennessee</option>
-        <option value="TX">Texas</option>
-        <option value="UT">Utah</option>
-        <option value="VT">Vermont</option>
-        <option value="VA">Virginia</option>
-        <option value="WA">Washington</option>
-        <option value="WV">West Virginia</option>
-        <option value="WI">Wisconsin</option>
-        <option value="WY">Wyoming</option>
-    </select>
+  <!-- <div class="container-fluid"> -->
+    <div class="StateForm">
+      <!-- <div class= "row"> -->
+        <div class="form-group col-lg-4 col-md-4 col-sm-12">
+        <label for="name">Select a state</label>
+        <select class="form-control" v-model="name">
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+        </select>
+        <div class="form-group">
+          <label for="year">What year did you visit?</label>
+          <input class="form-control" type="text" v-model="year" />
+        </div>
+        <div class="form-group">
+          <label for="days">How many days did you stay?</label>
+          <input class="form-control" type="text" v-model="days" />
+        </div>
+        <div class="form-group">
+          <label for="notes">Add some memos</label>
+          <textarea class="form-control" v-model="notes"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="rating">Give it a rating!</label>
+          <input class="form-control" v-model="rating" />
+        </div>
+        <button class="btn btn-primary" @click="create" :disabled="loading">Create State</button>
+      </div>
+      <div class="col-lg-8 col-md-8 col-sm-12"></div>
     </div>
-    <div class="form-group">
-      <label for="year">What year did you visit?</label>
-      <input class="form-control" type="text" v-model="year" />
-    </div>
-    <div class="form-group">
-      <label for="days">How many days did you stay?</label>
-      <input class="form-control" type="text" v-model="days" />
-    </div>
-    <div class="form-group">
-      <label for="notes">Add some memos</label>
-      <textarea class="form-control" v-model="notes"></textarea>
-    </div>
-    <div class="form-group">
-      <label for="rating">Give it a rating!</label>
-      <input class="form-control" v-model="rating" />
-    </div>
-    <button class="btn btn-primary" @click="create" :disabled="loading">Create State</button>
-  </div>
+  <!-- </div> -->
+<!-- </div> -->
 </template>
 
 <script>
@@ -143,6 +148,10 @@ export default {
   .StateForm {
     padding-top: 20px;
     padding-bottom: 30px;
+    float: left; width: 100%;
+    position: relative;
+    display: inline-block;
+
   }
 
 </style>
